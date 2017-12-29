9b89735010eb12ff69875dadcd07b5667bb29786
Yannis Spyrou <js15js@gmail.com>
2017-12-29 16:41:01 +0000

    Update custom.php

--- app/custom.php
+++ app/custom.php
@@ -1,7 +1,15 @@
 <?php 
-// Add SVG in Wordpress
-function cc_mime_types($mimes) {
-  $mimes['svg'] = 'image/svg+xml';
-  return $mimes;
+// Allow uploads of SVGs
+function add_svg_to_upload_mimes( $upload_mimes ) {
+	$upload_mimes['svg'] = 'image/svg+xml';
+	$upload_mimes['svgz'] = 'image/svg+xml';
+	return $upload_mimes;
 }
-add_filter('upload_mimes', 'cc_mime_types');
+add_filter( 'upload_mimes', 'add_svg_to_upload_mimes', 10, 1 );
+
+
+// Hide front-end admin menu when logged in
+add_action( 'init', 'disable_admin_bar', 1 );
+function disable_admin_bar() {
+    add_filter( 'show_admin_bar', '__return_false' );
+}
